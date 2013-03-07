d746c343629ecf6b582f793163c5052803bf4096
Justin J. Novack <jnovack@gmail.com>
2013-03-06 21:30:35 -0500

    Create lifxbulb page

--- lifxbulb.js
+++ lifxbulb.js
@@ -10,10 +10,13 @@ var     net = require('net');
 var  Schema = require('protobuf').Schema;
 var  schema = new Schema(fs.readFileSync('protobuf/lifx.desc'));
 var Command = schema['lifx.Command'];
+var    app = require('http').createServer(browser);
+var     io = require('socket.io').listen(app);
 var       r = 0,
           g = 0,
           b = 0,
           n = 0;
+io.set('log level', 1);
 
 var lifxbulb = net.createServer( function (socket) {
     socket.on('data', function(data) {
@@ -28,6 +31,7 @@ var lifxbulb = net.createServer( function (socket) {
         r = command.red;
         g = command.green;
         b = command.blue;
+        io.sockets.emit('color', { red: r, green: g, blue: b });
     });
 });
 lifxbulb.listen(5139);
@@ -35,11 +39,29 @@ lifxbulb.listen(5139);
 /*
  * Browser Based Debug
  */
-
-var app = require('http').createServer(browser);
 app.listen(8008);
 
 function browser (req, res) {
-    res.writeHead(200, { 'Content-type': 'text/html' });
-    res.end("<html><body style='background-color: rgb("+r+','+g+','+b+");'></body></html>\n");
+    var s = fs.createReadStream('./www/lifxbulb.html');
+    s.on('error', function () {
+        req.writeHead(404);
+        req.end();
+    })
+    s.once('fd', function () {req.writeHead(200);});
+    s.pipe(res);
 }
+
+/*
+io.sockets.on('connection', function (socket) {
+    socket.on('data', function (data) {
+        num++;
+        console.log("Message "+num+": "+data);
+        var serial = Command.serialize(data);
+        conn.write(serial);
+    });
+
+    socket.on('disconnect', function () {
+        // Placeholder
+    });
+});
+*/
