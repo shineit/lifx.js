e4bdbf3f449f2b902afd81c3066f135bc5b79764
Justin J. Novack <jnovack@parxcasino.com>
2013-03-07 11:33:57 -0500

    Reorganize structure and layout
    [ci skip]

--- lifx.js
+++ lifx.js
@@ -1,23 +1,52 @@
 #!/usr/bin/node
 /*
  * lifxjs.js - LIFX Color Picker for NodeJS
+ *
+ * Front-end for lifx.js control.
  */
 
 var    app = require('http').createServer(browser);
 var     io = require('socket.io').listen(app);
-var    url = require('url');
 var     fs = require('fs');
 var    net = require('net');
+var    url = require('url');
+var   util = require('util');
+var Schema = require('protobuf').Schema;
+var schema = new Schema(fs.readFileSync('protobuf/lifx.desc'));
 var    num = 0;
 
-var Schema = require('protobuf').Schema;
-var conn = net.createConnection('5139', '127.0.0.1'); // Testing Port
 
-var schema = new Schema(fs.readFileSync('protobuf/lifx.desc'));
+/* This is currently sending data to the ./lifxbulb.js application
+ * which requires to be running prior to starting this application.
+ * In the future, this should send to an actual lifx bulb.
+ */
+var lifx = net.createConnection('5139', '127.0.0.1');
+
 
 io.set('log level', 1);
 var Command = schema['lifx.Command'];
 app.listen(8000);
+
+/* Socket (Back End)
+ * Process the incoming color data and send to the lifx bulb
+ */
+io.sockets.on('connection', function (socket) {
+    socket.on('data', function (data) {
+        num++;
+        console.log("Message "+num+": " + util.inspect(data, false, null));
+        var serial = Command.serialize(data);
+        lifx.write(serial);
+    });
+
+    socket.on('disconnect', function () {
+        // Placeholder
+    });
+});
+
+
+/* Browser (Front End)
+ * Give the user a pretty interface to control the color
+ */
 function browser (req, res) {
   var request = url.parse(req.url, true);
   var action = request.pathname;
@@ -27,6 +56,7 @@ function browser (req, res) {
      res.writeHead(200, {'Content-Type': 'image/png' });
      res.end(img, 'binary');
   } else if (action == '/socket.io.min.js') {
+    // TODO: This is not working correctly, currently file is included in index.html
     var s = fs.createReadStream('./socket.io.min.js');
     s.on('error', function () {
         req.writeHead(404);
@@ -43,16 +73,3 @@ function browser (req, res) {
      res.end(html);
   }
 }
-
-io.sockets.on('connection', function (socket) {
-    socket.on('data', function (data) {
-        num++;
-        console.log("Message "+num+": "+data);
-        var serial = Command.serialize(data);
-        conn.write(serial);
-    });
-
-    socket.on('disconnect', function () {
-        // Placeholder
-    });
-});
