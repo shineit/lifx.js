99be78559c545aac803720b3d262205844d2b9f3
Justin J. Novack <jnovack@parxcasino.com>
2013-03-08 11:44:48 -0500

    Reorg'd some code for speed and lint

--- lifx.js
+++ lifx.js
@@ -5,6 +5,7 @@
  * Front-end for lifx.js control.
  */
 
+var browser;
 var    app = require('http').createServer(browser);
 var     io = require('socket.io').listen(app);
 var     fs = require('fs');
@@ -22,7 +23,6 @@ var    num = 0;
  */
 var lifx = net.createConnection('5139', '127.0.0.1');
 
-
 io.set('log level', 1);
 var Command = schema['lifx.Command'];
 app.listen(8000);
@@ -31,54 +31,56 @@ app.listen(8000);
  * Process the incoming color data and send to the lifx bulb
  */
 io.sockets.on('connection', function (socket) {
-    socket.on('data', function (data) {
-        num++;
-        console.log("Message "+num+": " + util.inspect(data, false, null));
-        var serial = Command.serialize(data);
-        lifx.write(serial);
-    });
+  socket.on('data', function (data) {
+    num++;
+    console.log("Message " + num + ": " + util.inspect(data, false, null));
+    var serial = Command.serialize(data);
+    lifx.write(serial);
+  });
 
-    socket.on('disconnect', function () {
-        // Placeholder
-    });
+  socket.on('disconnect', function () {
+    // Placeholder
+  });
 });
 
 
 /* Browser (Front End)
  * Give the user a pretty interface to control the color
  */
-function browser (req, res) {
-  var request = url.parse(req.url, true);
-  var action = request.pathname;
+function browser(req, res) {
+  var request = url.parse(req.url, true),
+    action = request.pathname,
+    file = false,
+    mime = false,
+    s = false;
 
-  if (action == '/color-picker.png') {
-     var img = fs.readFileSync('./www/color-picker.png');
-     res.writeHead(200, {'Content-Type': 'image/png' });
-     res.end(img, 'binary');
-  } else if (action == '/bootstrap.min.css') {
+  if (action === '/color-picker.png') {
+    file = "./www/color-picker.png";
+    mime = "image/png";
+  } else if (action === '/bootstrap.min.css') {
+    file = "./www/bootstrap.min.css";
+    mime = "text/css";
+  } else if (action === '/socket.io.min.js') {
     // TODO: This is not working correctly, currently file is included in index.html
-    var s = fs.createReadStream('./www/bootstrap.min.css');
+    file = './socket.io.min.js';
+    mime = "text/javascript";
+  } else if (action === '/') {
+    file = "./www/index.html";
+    mime = "text/html";
+  }
+
+  if (file && mime) {
+    s = fs.createReadStream(file);
     s.on('error', function () {
-        res.writeHead(404);
-        res.end();
-    })
-    s.once('fd', function () {req.writeHead(200, { 'Content-Type':'text/css'});});
+      res.writeHead(404);
+      res.end();
+    });
+    s.once('fd', function () {
+      req.writeHead(200, { 'Content-Type' : mime });
+    });
     s.pipe(res);
-  } else if (action == '/socket.io.min.js') {
-    // TODO: This is not working correctly, currently file is included in index.html
-    var s = fs.createReadStream('./socket.io.min.js');
-    s.on('error', function () {
-        res.writeHead(404);
-        res.end();
-    })
-    s.once('fd', function () {req.writeHead(200, { 'Content-Type':'text/javascript'});});
-    s.pipe(res);
-  } else if (action == '/favicon.ico') {
-     res.writeHead(404);
-     res.end();
   } else {
-     var html = fs.readFileSync('./www/index.html');
-     res.writeHead(200, {'Content-Type': 'text/html' });
-     res.end(html);
+    res.writeHead(404);
+    res.end();
   }
 }
